e0448429ca65e48ddda8d3573b6e5aa32c57046a
alaguelo <alaguelo@ucsd.edu>
2023-01-25 16:10:01 -0800

    Update 1
    fixed reverseInPlace mirror bug, fixed reversed wrong array bug

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,23 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlace2(){
+    int[] input = {10,9,8,7,6};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{6,7,8,9,10}, input);
+  }
+
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed2(){
+    int[] input1 = {1,2,3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input1));
   }
 }
